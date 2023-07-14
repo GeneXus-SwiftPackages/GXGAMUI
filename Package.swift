80a0fb49c88af357653c878d6c4c32f6c006e373
genexusbot <builder@genexus.com>
2023-07-14 16:28:36 -0300

    Package version: 1.0.0-preview.20230714192150

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXGAMUI",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXGAMUI",
+			targets: ["GXGAMUIWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "1.0.0-preview.20230714192150"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXObjectsModel.git", exact: "1.0.0-preview.20230714192150")
+	],
+	targets: [
+		.target(name: "GXGAMUIWrapper",
+				dependencies: [
+					"GXGAMUI",
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .watchOS, .tvOS])),
+					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.iOS, .tvOS])),
+					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS, .tvOS])),
+					.product(name: "GXGAM", package: "GXGAM", condition: .when(platforms: [.iOS, .watchOS, .tvOS])),
+					.product(name: "GXObjectsModel", package: "GXObjectsModel", condition: .when(platforms: [.iOS, .tvOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXGAMUI",
+			url: "https://pkgs.genexus.dev/iOS/preview/GXGAMUI-1.0.0-preview.20230714192150.xcframework.zip",
+			checksum: "39953ad1bef38516593a8c183b0d68aae4fda9657d07ea7945b12ae5b3fe9764"
+		)
+	]
+)
