ee796c9a7c816ae25cce8aee9843e6c31434dc00
Build Er <builder@macmini6.local>
2023-09-13 12:06:04 -0300

    Package version: 1.2.0-rc.25

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXGAMUIWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.2.0-rc.15"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "1.2.0-rc.15")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.2.0-rc.25"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "1.2.0-rc.25")
 	],
 	targets: [
 		.target(name: "GXGAMUIWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXGAMUI",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXGAMUI-1.2.0-rc.15.xcframework.zip",
-			checksum: "5f5010e99bfe2055f4816ba8122b689815d52b54d0629c8c01d9ba00b12ec988"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXGAMUI-1.2.0-rc.25.xcframework.zip",
+			checksum: "a8eeb66dc8200b65fc5463d3c8c1bf8aed8786c0f1fe7afabbe95c1f0c6fb298"
 		)
 	]
 )
