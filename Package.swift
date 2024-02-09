ae1e3d8208a95b6eb00a5371114f18f0f8c4e9ff
genexusbot <builder@genexus.com>
2024-02-09 17:07:04 -0300

    Package version: 1.5.0-beta.2

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXGAMUIWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.4.0-beta.20"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "1.4.0-beta.20")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.5.0-beta.2"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "1.5.0-beta.2")
 	],
 	targets: [
 		.target(name: "GXGAMUIWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXGAMUI",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXGAMUI-1.4.0-beta.20.xcframework.zip",
-			checksum: "4613a767d97f37c725f989a84666180b75877add142790fb4ab9921fb423f580"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXGAMUI-1.5.0-beta.2.xcframework.zip",
+			checksum: "6501e85d9cc3048a17d525af43d705285ef08bc92acdce100a2beea5060e9633"
 		)
 	]
 )
