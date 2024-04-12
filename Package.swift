1744dfff35a053cf649b52db3c931739a55cc70c
Build Er <builder@macmini6.local>
2024-04-12 10:50:03 -0300

    Package version: 1.6.0-beta.2

--- Package.swift
+++ Package.swift
@@ -3,28 +3,28 @@ import PackageDescription
 
 let package = Package(
 	name: "GXGAMUI",
-	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0")],
+	platforms: [.iOS("12.0"), .watchOS("9.0"), .tvOS("12.0"), .visionOS("1.0")],
 	products: [
 		.library(
 			name: "GXGAMUI",
 			targets: ["GXGAMUIWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.6.0-beta.1"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "1.6.0-beta.1")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.6.0-beta.2"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "1.6.0-beta.2")
 	],
 	targets: [
 		.target(name: "GXGAMUIWrapper",
 				dependencies: [
 					"GXGAMUI",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .watchOS, .tvOS])),
-					.product(name: "GXGAM", package: "GXGAM", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .watchOS, .tvOS, .visionOS])),
+					.product(name: "GXGAM", package: "GXGAM", condition: .when(platforms: [.iOS, .watchOS, .tvOS, .visionOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXGAMUI",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXGAMUI-1.6.0-beta.1.xcframework.zip",
-			checksum: "e165c798bb22d3a6c57df7bfe90633f6f83a2b387c458ae30c4a4bb134f030a9"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXGAMUI-1.6.0-beta.2.xcframework.zip",
+			checksum: "53d7c34835aa8556f32d65f3f47d15cb664e5edbd69c81744f343966d12fe16e"
 		)
 	]
 )
