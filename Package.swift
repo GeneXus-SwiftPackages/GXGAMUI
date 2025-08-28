2a22a3716487118103de7812de687388a1634450
The iOS Team @ GeneXus <builder@genexus.com>
2025-08-28 09:20:01 -0400

    Package version: 3.2.0-beta.40

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXGAMUIWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.2.0-beta.39"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "3.2.0-beta.39")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.2.0-beta.40"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "3.2.0-beta.40")
 	],
 	targets: [
 		.target(name: "GXGAMUIWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXGAMUI",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXGAMUI-3.2.0-beta.39.xcframework.zip",
-			checksum: "d2f6178e84d9bab680ff207f19159c39770aec54b65370f6f51db9d0f795913b"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXGAMUI-3.2.0-beta.40.xcframework.zip",
+			checksum: "302b54efb0e52a881b2063899aa1ac3a1b54062b17ffc9aa8309346a1cd3c2af"
 		)
 	]
 )
